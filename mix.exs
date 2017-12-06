c70a2f2e5eb11fffa1302bf86fd75f2d7ffd76b8
Fernando Tapia Rico <fertapric@gmail.com>
2017-12-06 20:17:28 +0100

    Remove :runtime configuration
    Start those applications automatically if they need to.

--- mix.exs
+++ mix.exs
@@ -31,10 +31,10 @@ defmodule AsyncWith.Mixfile do
 
   defp deps do
     [
-      {:credo, "~> 0.8.10", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5.1", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.18.1", only: :docs, runtime: false},
-      {:inch_ex, ">= 0.0.0", only: :docs, runtime: false}
+      {:credo, "~> 0.8.10", only: :dev},
+      {:dialyxir, "~> 0.5.1", only: :dev},
+      {:ex_doc, "~> 0.18.1", only: :docs},
+      {:inch_ex, ">= 0.0.0", only: :docs}
     ]
   end
 
