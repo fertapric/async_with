b1aa7a93801e5c290940b0953da63780e5c4d730
Fernando Tapia Rico <fertapric@gmail.com>
2018-03-30 14:47:07 +0200

    Update .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
-  inputs: ["mix.exs", ".formatter.exs", ".credo.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["{mix,.formatter,.credo}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: [async: 1, async: 2],
   export: [locals_without_parens: [async: 1, async: 2]]
 ]
